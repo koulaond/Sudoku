8f4fd9490f11f7d9df6e840978ed48c79c7602ae
Ondrej Koula <koulaond@fel.cvut.cz>
2017-10-02 01:09:39 +0200

    Backtracking implementation - DONE (unit tests missing)

--- src/main/java/generator/backtracking/BacktrackingGenerator.java
+++ src/main/java/generator/backtracking/BacktrackingGenerator.java
@@ -2,12 +2,19 @@ package generator.backtracking;
 
 import generator.Generator;
 
+import java.util.*;
+
 public class BacktrackingGenerator implements Generator {
 
-    private final int[][] ground = new int[9][9];
+    private final Point[][] ground = new Point[9][9];
     private final Box[][] boxes = new Box[3][3];
 
     public BacktrackingGenerator() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                ground[i][j] = new Point(i, j);
+            }
+        }
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 boxes[i][j] = new Box(i, j, ground);
@@ -15,19 +22,90 @@ public class BacktrackingGenerator implements Generator {
         }
     }
 
-    private boolean recursiveFill(int boxRow, int boxColumn, int value) {
-        Box actualBox = boxes[boxRow][boxColumn];
-        int[][] actualBoxEmptyFields = actualBox.getEmptyFields();
+    private boolean recursiveFill(Box box, Integer value) {
+        ArrayList<Point> actualBoxPossibleFields = box.getEmptyFields();
 
-        boolean[][] triedFields = new boolean[3][3];
+        if (!actualBoxPossibleFields.isEmpty()) {
+            Collections.shuffle(actualBoxPossibleFields);
+            Iterator<Point> emptyFieldsIterator = actualBoxPossibleFields.iterator();
+            while (emptyFieldsIterator.hasNext()) {
+                Point nextField = emptyFieldsIterator.next();
+                if (!isSatisfied(nextField, value)) {
+                    continue;
+                }
+                nextField.setValue(value);
+                Box nextBox = nextBox(box.getBoxRow(), box.getBoxColumn(), value);
+                if (nextBox == null) {
+                    return true;
+                }
+                Integer nextValue = nextValue(box.getBoxRow(), box.getBoxColumn(), value);
+                boolean nextFill = recursiveFill(nextBox, nextValue);
+                if (nextFill == true) {
+                    return true;
+                } else {
+                    nextField.setValue(0);
+                }
+            }
+        }
+        if (box.isLast() && box.isFull()) {
+            return true;
+        } else return false;
 
-        if (actualBox.isLast() && actualBox.isFull()) return true;
-        else return false;
+    }
+
+    private boolean isSatisfied(Point field, final Integer value) {
+        List<Point> crossFields = getCrossFields(field);
+        return !crossFields.stream()
+                .anyMatch(actual -> actual.getValue() == value);
+    }
+
+    private List<Point> getCrossFields(Point field) {
+        int rowCoord = field.getRow();
+        int columnCoord = field.getColumn();
+        List<Point> crossFields = new ArrayList<>();
+
+        for (int i = 0; i < 9; i++) {
+            if (i != rowCoord) crossFields.add(ground[i][columnCoord]);
+            if (i != columnCoord) crossFields.add(ground[rowCoord][i]);
+        }
+        return crossFields;
+    }
+
+    private Box nextBox(Integer boxRow, Integer boxColumn, Integer valueFor) {
+        if (boxRow == 2 && boxColumn == 2 && valueFor == 9) return null;
+        else if (boxRow == 2 && boxColumn == 2) return boxes[0][0];
+        else if (boxColumn == 2) return boxes[boxRow + 1][0];
+        else return boxes[boxRow][boxColumn + 1];
+    }
+
+    private Integer nextValue(Integer boxRow, Integer boxColumn, Integer valueFor) {
+        if (boxRow == 2 && boxColumn == 2 && valueFor == 9) return null;
+        else if (boxRow == 2 && boxColumn == 2) ++valueFor;
+        return valueFor;
     }
 
     public int[][] generate() {
-        recursiveFill(0, 0, 1);
-        return ground;
+        recursiveFill(boxes[0][0], 1);
+        print();
+        return null;
+    }
+
+    private void print() {
+        for (int i = 0; i < 9; i++) {
+            if (i % 3 == 0) {
+                System.out.println();
+            }
+            for (int j = 0; j < 9; j++) {
+                if (j % 3 == 0) System.out.print(" ");
+                System.out.print(ground[i][j].getValue() + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        new BacktrackingGenerator().generate();
     }
 
 }
--- src/main/java/generator/backtracking/Box.java
+++ src/main/java/generator/backtracking/Box.java
@@ -4,53 +4,52 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Box {
-    private final int boxRow;
-    private final int boxColumn;
-    private final int[][] ground;
+    private final Integer boxRow;
+    private final Integer boxColumn;
+    private final Point[][] ground;
 
-    public Box(int boxRow, int boxColumn, int[][] ground) {
+    public Box(Integer boxRow, Integer boxColumn, Point[][] ground) {
         this.boxRow = boxRow;
         this.boxColumn = boxColumn;
         this.ground = ground;
     }
 
-    public int getBoxRow() {
+    public Integer getBoxRow() {
         return boxRow;
     }
 
-    public int getBoxColumn() {
+    public Integer getBoxColumn() {
         return boxColumn;
     }
 
-    public int[][] getGround() {
+    public Point[][] getGround() {
         return this.ground;
     }
 
-    public void setNumber(int row, int col, int value) {
-        this.ground[this.boxRow * 3 + row][this.boxColumn * 3 + col] = value;
+    public void setNumber(Integer row, Integer col, Integer value) {
+        this.ground[this.boxRow * 3 + row][this.boxColumn * 3 + col].setValue(value);
     }
 
     public boolean isFull() {
-        return getEmptyFields().length == 0;
+        return getEmptyFields().isEmpty();
     }
 
     public boolean isLast() {
         return boxRow == 2 && boxColumn == 2;
     }
 
-    public int[][] getEmptyFields() {
-        ArrayList<Integer[]> emptyFields = new ArrayList<Integer[]>();
-        int startX = boxRow * 3;
-        int startY = boxColumn * 3;
+    public ArrayList<Point> getEmptyFields() {
+        ArrayList<Point> emptyFields = new ArrayList();
+        Integer startX = boxRow * 3;
+        Integer startY = boxColumn * 3;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (ground[startX + i][startY + j] == 0) {
-                    int xCoord = startX + i;
-                    int yCoord = startY + j;
-                    emptyFields.add(new Integer[]{xCoord, yCoord});
+                Point actual = ground[startX + i][startY + j];
+                if (actual.getValue() == 0) {
+                    emptyFields.add(actual);
                 }
             }
         }
-        return emptyFields.toArray(new int[emptyFields.size()][]);
+        return emptyFields;
     }
 }
